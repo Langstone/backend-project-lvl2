34e9e7ca76771976caabb26adb41cf85917b2b57
Langstone <kalipsomart@gmail.com>
2021-02-23 00:06:13 +0600

    added arguments <filepath1>, <filepath2> and option format

--- gendiff.js
+++ gendiff.js
@@ -7,7 +7,8 @@ genDiff.version('0.0.1');
 
 genDiff
 .description('Compares two configuration files and shows a difference.')
-
+.arguments('<filepath1> <filepath2>')
+.option('-f, --format [type]', 'output format')
 
 genDiff.parse(process.agrv);
 
