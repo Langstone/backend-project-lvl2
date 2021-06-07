26482fa57e33e90496ef7e2761dcd1dd816d78ff
Langstone <kalipsomart@gmail.com>
2021-06-07 22:27:22 +0600

    test

--- formatters/json.js
+++ formatters/json.js
@@ -1,62 +1,63 @@
 import _ from 'lodash';
 
-function simpleRecursion(obj, lvl) {
-  let result = [];
-  const indent = '  ';
-  Object.keys(obj).forEach((key) => {
-    const value = obj[key];
-    if (!_.isPlainObject(value)) {
-      result.push(`${indent.repeat(lvl)}${key}: ${value}`);
-    } else {
-      result.push(`${indent.repeat(lvl)}${key}: {`);
-      result = result.concat(simpleRecursion(value, lvl + 2));
-    }
-  });
-  result.push(`${indent.repeat(lvl - 2)}}`);
-  return result;
-}
+// function simpleRecursion(obj, lvl) {
+//   let result = [];
+//   const indent = '  ';
+//   Object.keys(obj).forEach((key) => {
+//     const value = obj[key];
+//     if (!_.isPlainObject(value)) {
+//       result.push(`${indent.repeat(lvl)}${key}: ${value}`);
+//     } else {
+//       result.push(`${indent.repeat(lvl)}${key}: {`);
+//       result = result.concat(simpleRecursion(value, lvl + 2));
+//     }
+//   });
+//   result.push(`${indent.repeat(lvl - 2)}}`);
+//   return result;
+// }
 
-function stringifyValue(value, lvl) {
-  if (_.isPlainObject(value)) {
-    const result = simpleRecursion(value, lvl + 2).join('\n');
-    return `{\n${result}`;
-  }
-  return `${value}`;
-}
+// function stringifyValue(value, lvl) {
+//   if (_.isPlainObject(value)) {
+//     const result = simpleRecursion(value, lvl + 2).join('\n');
+//     return `{\n${result}`;
+//   }
+//   return `${value}`;
+// }
 
-function complexRecursion(obj, lvl = 2) {
-  let result = [];
-  const indent = '  ';
-  Object.keys(obj).forEach((key) => {
-    const value = obj[key];
-    if ('__equal__' in value) {
-      if (value.__equal__ === true) {
-        result.push(`${indent.repeat(lvl)}${key}: ${stringifyValue(value.__before__, lvl)}`);
-      } else if (value.__equal__ === false) {
-        if (value.__before__ === undefined) {
-          result.push(`${indent.repeat(lvl - 1)}+ ${key}: ${stringifyValue(value.__after__, lvl)}`);
-        } else if (value.__after__ === undefined) {
-          result.push(`${indent.repeat(lvl - 1)}- ${key}: ${stringifyValue(value.__before__, lvl)}`);
-        } else {
-          result.push(`${indent.repeat(lvl - 1)}- ${key}: ${stringifyValue(value.__before__, lvl)}`);
-          result.push(`${indent.repeat(lvl - 1)}+ ${key}: ${stringifyValue(value.__after__, lvl)}`);
-        }
-      }
-    } else {
-      result.push(`${indent.repeat(lvl)}${key}: {`);
-      const recursionResult = complexRecursion(value, lvl + 2);
-      result = result.concat(recursionResult);
-      result.push(`${indent.repeat(lvl)}}`);
-    }
-  });
-  return result;
-}
+// function complexRecursion(obj, lvl = 2) {
+//   let result = [];
+//   const indent = '  ';
+//   Object.keys(obj).forEach((key) => {
+//     const value = obj[key];
+//     if ('__equal__' in value) {
+//       if (value.__equal__ === true) {
+//         result.push(`${indent.repeat(lvl)}${key}: ${stringifyValue(value.__before__, lvl)}`);
+//       } else if (value.__equal__ === false) {
+//         if (value.__before__ === undefined) {
+//           result.push(`${indent.repeat(lvl - 1)}+ ${key}: ${stringifyValue(value.__after__, lvl)}`);
+//         } else if (value.__after__ === undefined) {
+//           result.push(`${indent.repeat(lvl - 1)}- ${key}: ${stringifyValue(value.__before__, lvl)}`);
+//         } else {
+//           result.push(`${indent.repeat(lvl - 1)}- ${key}: ${stringifyValue(value.__before__, lvl)}`);
+//           result.push(`${indent.repeat(lvl - 1)}+ ${key}: ${stringifyValue(value.__after__, lvl)}`);
+//         }
+//       }
+//     } else {
+//       result.push(`${indent.repeat(lvl)}${key}: {`);
+//       const recursionResult = complexRecursion(value, lvl + 2);
+//       result = result.concat(recursionResult);
+//       result.push(`${indent.repeat(lvl)}}`);
+//     }
+//   });
+//   return result;
+// }
 
 function jsonFormatter(obj) {
-  let result = ['{'];
-  result = result.concat(complexRecursion(obj));
-  result.push('}');
-  return result.join('\n');
+//   let result = ['{'];
+//   result = result.concat(complexRecursion(obj));
+//   result.push('}');
+//   return result.join('\n');
+  return 1;
 }
 
 export default jsonFormatter;
