2c398070653dfb0781ad9429569d452157d2285f
Langstone <kalipsomart@gmail.com>
2021-06-08 20:52:12 +0600

    refactoring indents

--- formatters/json.js
+++ formatters/json.js
@@ -6,9 +6,9 @@ function simpleRecursion(obj, lvl) {
   Object.keys(obj).forEach((key) => {
     const value = obj[key];
     if (!_.isPlainObject(value)) {
-      result.push(`${indent.repeat(lvl + 1)} ${key}: ${value}`);
+      result.push(`${indent.repeat(lvl + 1)}${key}: ${value}`);
     } else {
-      result.push(`${indent.repeat(lvl + 2)} ${key}: {`);
+      result.push(`${indent.repeat(lvl + 2)}${key}: {`);
       result = result.concat(simpleRecursion(value, lvl + 2));
     }
   });
@@ -33,22 +33,22 @@ function complexRecursion(obj, lvl = 1) {
     const value = obj[key];
     if ('__equal__' in value) {
       if (value.__equal__ === true) {
-        result.push(`${indent.repeat(lvl + 1)} ${key}: ${stringifyValue(value.__before__, lvl)}`);
+        result.push(`${indent.repeat(lvl + 1)}${key}: ${stringifyValue(value.__before__, lvl)}`);
       } else if (value.__equal__ === false) {
         if (value.__before__ === undefined) {
-          result.push(`${repeatIndent} + ${key}: ${stringifyValue(value.__after__, lvl)}`);
+          result.push(`${repeatIndent}+ ${key}: ${stringifyValue(value.__after__, lvl)}`);
         } else if (value.__after__ === undefined) {
-          result.push(`${repeatIndent} - ${key}: ${stringifyValue(value.__before__, lvl)}`);
+          result.push(`${repeatIndent}- ${key}: ${stringifyValue(value.__before__, lvl)}`);
         } else {
-          result.push(`${repeatIndent} - ${key}: ${stringifyValue(value.__before__, lvl)}`);
-          result.push(`${repeatIndent} + ${key}: ${stringifyValue(value.__after__, lvl)}`);
+          result.push(`${repeatIndent}- ${key}: ${stringifyValue(value.__before__, lvl)}`);
+          result.push(`${repeatIndent}+ ${key}: ${stringifyValue(value.__after__, lvl)}`);
         }
       }
     } else {
-      result.push(`${indent.repeat(lvl + 1)} ${key}: {`);
+      result.push(`${indent.repeat(lvl + 1)}${key}: {`);
       const recursionResult = complexRecursion(value, lvl + 2);
       result = result.concat(recursionResult);
-      result.push(`${indent.repeat(lvl + 1)} }`);
+      result.push(`${indent.repeat(lvl + 1)}}`);
     }
   });
   return result;
