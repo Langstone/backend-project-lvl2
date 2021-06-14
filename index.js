a80d14827e33aaa3e23f4da34dfd481110c26b19
Langstone <kalipsomart@gmail.com>
2021-06-14 14:17:26 +0600

    refactoring

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-import getFormatter from './formatters/index.js';
-import diffFiles from './diff.js';
-import parser from './parsers/parser.js';
+// import getFormatter from './formatters/index.js';
+// import diffFiles from './diff.js';
+// import parser from './parsers/parser.js';
 
-export default function compare(filepath1, filepath2) {
-  const formatter = getFormatter();
-  const obj1 = parser(filepath1);
-  const obj2 = parser(filepath2);
-  const diffObj = diffFiles(obj1, obj2);
-  const output = formatter(diffObj);
-  return output;
-}
+// export default function compare(filepath1, filepath2) {
+//   const formatter = getFormatter();
+//   const obj1 = parser(filepath1);
+//   const obj2 = parser(filepath2);
+//   const diffObj = diffFiles(obj1, obj2);
+//   const output = formatter(diffObj);
+//   return output;
+// }
+
+import genDiff from './gendiff.js';
+
+export default genDiff;
