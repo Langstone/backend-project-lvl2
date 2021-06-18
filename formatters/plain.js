95ab31709806642a2024d4dc23c78616ed42dd83
Langstone <kalipsomart@gmail.com>
2021-06-19 00:10:48 +0600

    refactoring eslint errors in plain

--- formatters/plain.js
+++ formatters/plain.js
@@ -4,40 +4,36 @@ function defineType(value) {
   if (typeof value === 'string') {
     return `'${value}'`;
   }
-
   return value;
 }
 
 function simpleRecursion(obj, oldKey = '') {
-  let result = [];
-  Object.keys(obj).forEach((key) => {
+  const result = Object.keys(obj).map((key) => {
     const value = obj[key];
     if (_.isPlainObject(value)) {
       if ('equal' in value) {
         if (value.equal === false) {
           if (value.before === undefined) {
             if (_.isPlainObject(value.after)) {
-              result.push(`Property '${oldKey}${key}' was added with value: [complex value]`);
-            } else {
-              result.push(`Property '${oldKey}${key}' was added with value: ${defineType(value.after)}`);
+              return `Property '${oldKey}${key}' was added with value: [complex value]`;
             }
-          } else if (value.after === undefined) {
-            result.push(`Property '${oldKey}${key}' was removed`);
-          } else if (_.isPlainObject(value.after)) {
-            result.push(`Property '${oldKey}${key}' was updated. From ${defineType(value.before)} to [complex value]`);
-          } else if (_.isPlainObject(value.before)) {
-            result.push(`Property '${oldKey}${key}' was updated. From [complex value] to ${defineType(value.after)}`);
-          } else {
-            result.push(`Property '${oldKey}${key}' was updated. From ${defineType(value.before)} to ${defineType(value.after)}`);
+            return `Property '${oldKey}${key}' was added with value: ${defineType(value.after)}`;
+          } if (value.after === undefined) {
+            return `Property '${oldKey}${key}' was removed`;
+          } if (_.isPlainObject(value.after)) {
+            return `Property '${oldKey}${key}' was updated. From ${defineType(value.before)} to [complex value]`;
+          } if (_.isPlainObject(value.before)) {
+            return `Property '${oldKey}${key}' was updated. From [complex value] to ${defineType(value.after)}`;
           }
+          return `Property '${oldKey}${key}' was updated. From ${defineType(value.before)} to ${defineType(value.after)}`;
         }
-      } else {
-        const currentKey = `${oldKey}${key}.`;
-        result = result.concat(simpleRecursion(value, currentKey));
       }
+      const currentKey = `${oldKey}${key}.`;
+      return simpleRecursion(value, currentKey);
     }
+    return '';
   });
-  return result;
+  return _.flattenDeep(result.filter((e) => e));
 }
 
 function plainFormatter(obj) {
