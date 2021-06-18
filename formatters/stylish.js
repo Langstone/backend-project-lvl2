ebcc8f535b2ee2de50aad3024e74060489fabb75
Langstone <kalipsomart@gmail.com>
2021-06-18 23:38:24 +0600

    refactoring eslint errors in stylish

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,19 +1,15 @@
 import _ from 'lodash';
 
 function simpleRecursion(obj, lvl) {
-  let result = [];
   const indent = '  ';
-  Object.keys(obj).forEach((key) => {
+  const result = Object.keys(obj).map((key) => {
     const value = obj[key];
     if (!_.isPlainObject(value)) {
-      result.push(`${indent.repeat(lvl + 2)}${key}: ${value}`);
-    } else {
-      result.push(`${indent.repeat(lvl + 2)}${key}: {`);
-      result = result.concat(simpleRecursion(value, lvl + 2));
+      return [[`${indent.repeat(lvl + 2)}${key}: ${value}`]];
     }
+    return [[`${indent.repeat(lvl + 2)}${key}: {`], [simpleRecursion(value, lvl + 2)]];
   });
-  result.push(`${indent.repeat(lvl)}}`);
-  return result;
+  return _.flattenDeep([[_.flattenDeep(result)], [`${indent.repeat(lvl)}}`]]);
 }
 
 function stringifyValue(value, lvl) {
@@ -21,44 +17,33 @@ function stringifyValue(value, lvl) {
     const result = simpleRecursion(value, lvl + 1).join('\n');
     return `{\n${result}`;
   }
-
   return `${value}`;
 }
 
 function complexRecursion(obj, lvl = 1) {
-  let result = [];
   const indent = '  ';
   const repeatIndent = indent.repeat(lvl);
-  Object.keys(obj).forEach((key) => {
+  const result = Object.keys(obj).map((key) => {
     const value = obj[key];
     if ('equal' in value) {
       if (value.equal === true) {
-        result.push(`${indent.repeat(lvl + 1)}${key}: ${stringifyValue(value.before, lvl)}`);
-      } else if (value.equal === false) {
+        return `${indent.repeat(lvl + 1)}${key}: ${stringifyValue(value.before, lvl)}`;
+      } if (value.equal === false) {
         if (value.before === undefined) {
-          result.push(`${repeatIndent}+ ${key}: ${stringifyValue(value.after, lvl)}`);
-        } else if (value.after === undefined) {
-          result.push(`${repeatIndent}- ${key}: ${stringifyValue(value.before, lvl)}`);
-        } else {
-          result.push(`${repeatIndent}- ${key}: ${stringifyValue(value.before, lvl)}`);
-          result.push(`${repeatIndent}+ ${key}: ${stringifyValue(value.after, lvl)}`);
+          return `${repeatIndent}+ ${key}: ${stringifyValue(value.after, lvl)}`;
+        } if (value.after === undefined) {
+          return `${repeatIndent}- ${key}: ${stringifyValue(value.before, lvl)}`;
         }
+        return [[`${repeatIndent}- ${key}: ${stringifyValue(value.before, lvl)}`], [`${repeatIndent}+ ${key}: ${stringifyValue(value.after, lvl)}`]];
       }
-    } else {
-      result.push(`${indent.repeat(lvl + 1)}${key}: {`);
-      const recursionResult = complexRecursion(value, lvl + 2);
-      result = result.concat(recursionResult);
-      result.push(`${indent.repeat(lvl + 1)}}`);
     }
+    return [[`${indent.repeat(lvl + 1)}${key}: {`], [complexRecursion(value, lvl + 2)], [`${indent.repeat(lvl + 1)}}`]];
   });
-  return result;
+  return _.flattenDeep(result);
 }
 
 function generateDiff(obj) {
-  let result = ['{'];
-  result = result.concat(complexRecursion(obj));
-  result.push('}');
-  return result.join('\n');
+  return _.flattenDeep([['{'], [complexRecursion(obj)], ['}']]).join('\n');
 }
 
 export default generateDiff;
