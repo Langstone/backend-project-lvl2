432b83f5dd96a9da88c4c7303598ebefdb17d644
Langstone <kalipsomart@gmail.com>
2021-06-19 01:46:05 +0600

    fixed errors eslint in diff

--- diff.js
+++ diff.js
@@ -1,35 +1,32 @@
 import _ from 'lodash';
 
 function diffFiles(obj1, obj2) {
-  const result = {};
   const uniqKeys = _.uniq(_.concat(_.keys(obj1), _.keys(obj2))).sort();
 
-  uniqKeys.forEach((uniqKey) => {
-    const uniqKeyResult = {
-      equal: false,
-      after: undefined,
-      before: undefined,
-    };
-
+  const result = uniqKeys.map((uniqKey) => {
     if (obj1[uniqKey] !== undefined && obj2[uniqKey] !== undefined) {
       const recursionRequired = _.isPlainObject(obj1[uniqKey]) && _.isPlainObject(obj2[uniqKey]);
       if (recursionRequired) {
-        result[uniqKey] = diffFiles(obj1[uniqKey], obj2[uniqKey]);
-      } else {
-        uniqKeyResult.equal = obj1[uniqKey] === obj2[uniqKey];
-        uniqKeyResult.before = obj1[uniqKey];
-        uniqKeyResult.after = obj2[uniqKey];
-        result[uniqKey] = uniqKeyResult;
+        return [uniqKey, diffFiles(obj1[uniqKey], obj2[uniqKey])];
       }
-    } else if (obj1[uniqKey] !== undefined) {
-      uniqKeyResult.before = obj1[uniqKey];
-      result[uniqKey] = uniqKeyResult;
-    } else {
-      uniqKeyResult.after = obj2[uniqKey];
-      result[uniqKey] = uniqKeyResult;
+      return [uniqKey, {
+        equal: obj1[uniqKey] === obj2[uniqKey],
+        before: obj1[uniqKey],
+        after: obj2[uniqKey],
+      }];
+    } if (obj1[uniqKey] !== undefined) {
+      return [uniqKey, {
+        equal: false,
+        before: obj1[uniqKey],
+      }];
     }
+    return [uniqKey, {
+      equal: false,
+      after: obj2[uniqKey],
+    }];
   });
-  return result;
+
+  return Object.fromEntries(result);
 }
 
 export default diffFiles;
