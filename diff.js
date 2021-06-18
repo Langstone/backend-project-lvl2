bc84f99ee7c0bcf1dfdf26fc3f4adf746098f2f6
Langstone <kalipsomart@gmail.com>
2021-06-19 02:15:53 +0600

    refactoring diff.js

--- diff.js
+++ diff.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
+function getDiffPart(before, after) {
+  return { equal: before === after, before, after };
+}
+
 function diffFiles(obj1, obj2) {
   const uniqKeys = _.uniq(_.sortBy(_.concat(_.keys(obj1), _.keys(obj2))));
 
@@ -9,21 +13,11 @@ function diffFiles(obj1, obj2) {
       if (recursionRequired) {
         return [uniqKey, diffFiles(obj1[uniqKey], obj2[uniqKey])];
       }
-      return [uniqKey, {
-        equal: obj1[uniqKey] === obj2[uniqKey],
-        before: obj1[uniqKey],
-        after: obj2[uniqKey],
-      }];
+      return [uniqKey, getDiffPart(obj1[uniqKey], obj2[uniqKey])];
     } if (obj1[uniqKey] !== undefined) {
-      return [uniqKey, {
-        equal: false,
-        before: obj1[uniqKey],
-      }];
+      return [uniqKey, getDiffPart(obj1[uniqKey])];
     }
-    return [uniqKey, {
-      equal: false,
-      after: obj2[uniqKey],
-    }];
+    return [uniqKey, getDiffPart(undefined, obj2[uniqKey])];
   });
 
   return Object.fromEntries(result);
